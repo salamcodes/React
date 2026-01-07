bf38e8089ca5cac8a0918661b4753d648bad1e24
salamcodes <abdulsalampk550@gmail.com>
2026-01-07 21:00:06 +0500

    Theme switcher

--- theme-changer/src/components/Btn.jsx
+++ theme-changer/src/components/Btn.jsx
@@ -4,7 +4,7 @@ const ThemeButton = () => {
             className="px-4 py-2 rounded-lg
                  bg-gray-900 text-white
                  dark:bg-gray-100 dark:text-black
-                 transition-colors duration-300"
+                 transition-colors duration-300 cursor-pointer"
         >
             Toggle Theme
         </button>
